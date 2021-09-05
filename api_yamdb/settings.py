e153e7de76684052e885bc3791427ee707b92a8d
bitcoineazy <bitcoineazy@gmail.com>
2021-09-05 19:01:08 +0300

    postgresql

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
